bad904b719d01595932a6f57d373dada165ad792
BLAZ\blazz <orderbobproject@gmail.com>
2024-10-02 14:46:48 +0200

    no message

--- checksum_modulo_256/checksum.c
+++ checksum_modulo_256/checksum.c
@@ -9,6 +9,6 @@ __declspec(dllexport) int calculate_checksum(const char* buffer, size_t size) {
     for (size_t i = 0; i < size - 1; i++) {
         sum += (unsigned char)buffer[i];
     }
-
+    // Used modulo 256 for checksum (no need for %256, because we have unsigned char)
     return sum; // Return sum as unsigned char (wraps automatically)
 }
